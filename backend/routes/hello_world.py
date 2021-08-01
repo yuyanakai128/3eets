01cbf9a69fbebb3478890256a49a3e6a9f4b107d
kimotosenfa1975 <superdev195128@hotmail.com>
2021-08-01 09:37:35 +0900

    iroha flask docker

--- backend/routes/hello_world.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, make_response
-
-
-bp = Blueprint('hello_world', __name__, url_prefix='/api')
-
-
-@bp.route('/hello_world')
-def hello_world():
-    return make_response(jsonify(message='Hello World arigatou')), 200
